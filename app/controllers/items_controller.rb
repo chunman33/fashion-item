c2ec99c423315c52fbff74785b94b65ec490c926
EC2 Default User <ec2-user@ip-172-31-47-83.ap-northeast-1.compute.internal>
2022-11-29 05:54:19 +0000

    ユーザ編集機能

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -7,12 +7,17 @@ class ItemsController < ApplicationController
 
   def show
     @item = Item.find(params[:id])
+    @user = @item.user
   end
 
   def edit
+    @item = Item.find(params[:id])
   end
 
   def update
+    @item = Item.find(params[:id])
+    @item.update(item_params)
+    redirect_to item_path(@item)
   end
 
   def destroy
@@ -32,6 +37,8 @@ class ItemsController < ApplicationController
   def new
     @item = Item.new
   end
+  
+  private
   
   def item_params
     params.require(:item).permit(:genre_id, :image, :name, :description)
